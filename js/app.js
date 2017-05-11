83ebd0679dc014bdcf1a59a2ef98d7604495d02e
Michael Skotar <michaelskotar@gmail.com>
2017-05-11 07:49:04 -0400

    added the background animations with the color change.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation()
+$(document).ready(function(){
+
+    // Interval for changing the background
+    window.setInterval(changebackground, 500);
+
+
+});
+
+
+// Change the background function
+function changebackground(){
+    
+    var bodycolor = $("body").css('background-color');
+
+    //bodycolor.val("blue");
+    console.log(bodycolor);
+}
